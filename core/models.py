fe2754b92f51af247ee1befbb32b3654bc64603a
[Josh Shepherd] <[joshuashepherd877@gmail.com]>
2024-12-07 08:21:08 +0000

    add subuser model and relationship to restaurant

--- core/models.py
+++ core/models.py
@@ -12,3 +12,20 @@ class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # Foreign key linking to User
+    subusers = db.relationship('Subuser', backref='subuser')
+    
+class Subuser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id')) 
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    first_name = db.Column(db.String(55))
+    last_name = db.Column(db.String(55))
+    
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # rest_id
+    # type
+    # title
+    # created at
+    # expiry date
