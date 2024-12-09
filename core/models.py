500a126480e42460a7307b50d72250a8ec979fbb
[Josh Shepherd] <[joshuashepherd877@gmail.com]>
2024-12-09 07:39:10 +0000

    add user functions and enum types for doc categories and user roles

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
 from .extensions import db
+from sqlalchemy.types import Enum
+
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # User Model (Admins and Sub-users)
@@ -8,12 +10,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)  # Use Werkzeug for hashing
+    password_hash = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(10), nullable=False)  # 'admin' or 'sub-user'
-    
+
+    # Password methods
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     # Relationships
-    restaurants = db.relationship('Restaurant', backref='admin', lazy=True)  # For Admin's Restaurants
-    assignments = db.relationship('UserRestaurant', back_populates='user', lazy=True)  # Sub-user assignments
+    restaurants = db.relationship('Restaurant', backref='admin', lazy=True)
+    assignments = db.relationship('UserRestaurant', back_populates='user', lazy=True)
 
 # Restaurant Model
 class Restaurant(db.Model):
@@ -33,7 +42,7 @@ class UserRestaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
-    role = db.Column(db.String(50), nullable=True)  # Optional (e.g., 'viewer', 'editor')
+    role = db.Column(Enum('admin', 'sub-user', 'viewer', 'editor', name='user_roles'), nullable=False)
     
     # Relationships
     user = db.relationship('User', back_populates='assignments')
@@ -46,6 +55,8 @@ class Document(db.Model):
     name = db.Column(db.String(100), nullable=False)
     file_path = db.Column(db.String(200), nullable=False)  # Path to the stored document
     uploaded_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    uploaded_by = db.Column(db.String(100), nullable=False)
+    category = db.Column(Enum('Hygiene', 'Health & Safety', 'Training', 'Licenses', name='doc_category'), nullable=False)
     due_date = db.Column(db.DateTime, nullable=True)
     status = db.Column(db.String(20), default='active')  # e.g., 'active', 'expired'
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)  # Associated restaurant
