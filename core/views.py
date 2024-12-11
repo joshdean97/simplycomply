03b057a3e23264a2147523953f70ec2bf4f55787
[Josh Shepherd] <[joshuashepherd877@gmail.com]>
2024-12-11 11:46:40 +0000

    create upload to aws

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import current_user, login_required
+
+import boto3 
+import uuid
+
+from .models import Document
+from .extensions import db
 
 views = Blueprint('views', __name__)
 
@@ -24,9 +30,42 @@ def dashboard():
 def pricing():
     return render_template('pricing.html')
 
-@views.route('/upload/')
+@views.route('/upload/', methods=['POST', 'GET'])
 @login_required
 def upload():
+    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+    
+    def allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
+    
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        new_filename = uuid.uuid4().hex + '.' + uploaded_file.filename.rsplit('.', 1)[1].lower()    
+        
+        if not allowed_file(uploaded_file.filename):
+                return "File not allowed"
+        
+        s3 = boto3.resource('s3')
+        bucket_name = 'simply-comply'
+        
+        s3.Bucket(bucket_name).upload_fileobj(uploaded_file, new_filename)
+        
+        new_file = Document(
+            name = uploaded_file.filename,
+            file_path ='s3://{}/{}'.format(bucket_name, new_filename),
+            uploaded_by = current_user.name,
+            category = request.form.get('category'),
+            # due_date = request.form.get('due_date'),
+            # restaurant_id = current_user.restaurant_id  # Assuming restaurant_id is a foreign key in the User model.
+            restaurant_id = 1
+        )
+        
+        db.session.add(new_file)
+        db.session.commit()
+        flash('File uploaded successfully', 'success')
+        return redirect(url_for('views.dashboard'))
+            
     
     context = {
         'current_user': current_user
