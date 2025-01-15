ec3624219c6712b2acc830d825a0774b18f6575a
[Josh Shepherd] <[joshuashepherd877@gmail.com]>
2025-01-15 09:48:21 +0000

    add coming soon page

--- core/static/index.js
+++ core/static/index.js
@@ -116,3 +116,35 @@ closeMenu.addEventListener("click", () => {
   menuToggleIcon.classList.add("fa-bars");
   menuToggleIcon.classList.remove("fa-times");
 });
+
+// coming soon page
+
+// Set the date we're counting down to
+const countDownDate = new Date("Dec 31, 2025 23:59:59").getTime();
+
+// Update the count down every 1 second
+const countdownfunction = setInterval(() => {
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="days", "hours", "minutes", "seconds"
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.querySelector(".countdown").innerHTML = "We have launched!";
+    }
+}, 1000);
