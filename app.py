870a684521851f2c38826ce267479b34ceec6bb4
[Josh Shepherd] <[joshuashepherd877@gmail.com]>
2025-02-20 11:29:13 +0000

    add debugging for stripe

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from core.functions import create_database
 if __name__ == "__main__":
     app = create_app()
     create_database(app)
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=8080)
